0de1d9f8b970a691c462fd0d9d3d27f014e0452e
Sơn Nguyễn <sonnguyen@MacBook-Air-cua-Son.local>
2021-07-05 10:39:32 +0700

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+ 
+app.use(morgan('combined'))
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.listen(3000)
+
+
