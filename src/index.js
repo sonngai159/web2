7c3227b4fcd0e990d32da7c070b8565810504524
Sơn Nguyễn <sonnguyen@MacBook-Air-cua-Son.local>
2021-07-05 12:14:59 +0700

    setting up

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars');
+const app = express()
+ 
+app.engine('hbs', handlebars({
+  extname : '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// http logger
+app.use(morgan('combined'))
+
+app.get('/', function (req, res) {
+  res.send("Fuck Me");
+});
+
+app.get('/home', function (req, res) {
+  res.render('home');
+});
+
+
+app.listen(3000)
+
+ 
